8b24ac3efb81069d4c2c4939f6374650cfaeacdf
Nate Shih <nshih.85@gmail.com>
2017-01-19 01:38:05 -0500

    Final Commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shackburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoke Shack', FALSE);
